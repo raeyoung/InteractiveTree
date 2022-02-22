033d81a7aa7f202cc5040c161d00e4cc2923f7f5
raeyoung <raeyoung233@gmail.com>
2022-02-22 17:17:27 +0900

    나뭇가지 생성

--- src/js/tree.js
+++ src/js/tree.js
@@ -6,28 +6,46 @@ export class Tree {
         this.posX = posX;
         this.posY = posY;
         this.branches = []; 
+        this.depth = 5; // depth 추가
 
         this.init();
     }
     init() {
-        this.createBranch(this.posX, this.posY);
+        // 시작 각도는 -90도로 아래에서 위로 나무 기둥이 자라도록한다.
+        // 시작 depth는 0으로 준다.
+        this.createBranch(this.posX, this.posY, -90, 0);
         this.draw(this.ctx);
     }
 
-    // 나뭇가지 생성하기
-    createBranch(startX, startY) {
-        const len = 200;    // 나무기둥 길이
-        const endX = startX;
-        const endY = startY - len;  // 아래 -> 위 방향으로 그려주기 때문에 
-
+    // 나무 생성하기
+    createBranch(startX, startY, angle, depth) {
+        if (depth === this.depth) return;
+    
+        const len = 100; // 나무의 길이 100으로 변경
+        const endX = startX + this.cos(angle) * len;
+        const endY = startY + this.sin(angle) * len;
+    
         this.branches.push(new Branch(startX, startY, endX, endY));
-    }
+    
+        this.createBranch(endX, endY, angle - 30, depth + 1);
+        this.createBranch(endX, endY, angle + 30, depth + 1);
+      }
 
-    // 나뭇가지를 canvas 에 그리기 
+    // 나무를 canvas 에 그리기 
     draw(ctx) {
         for (let i = 0 ; i < this.branches.length ; i++) {
             this.branches[i].draw(ctx);
           }
     }
 
+    // 각도 관련 함수 추가
+    cos(angle) {
+        return Math.cos(this.degToRad(angle));
+    }
+    sin(angle) {
+        return Math.sin(this.degToRad(angle));
+    }
+    degToRad(angle) {
+        return (angle / 180.0) * Math.PI;
+    }
 }

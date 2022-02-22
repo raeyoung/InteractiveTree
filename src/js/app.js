1c33d31c3a28c7707cb440c1d2ec45fb8dcebce4
raeyoung <raeyoung233@gmail.com>
2022-02-22 16:53:12 +0900

    개발자도구를 통해 canvas 확인

---
+++ src/js/app.js
@@ -0,0 +1,33 @@
+class App {
+    constructor() {
+        // 캔버스 생성 및 랜더링
+        this.canvas = document.createElement('canvas');
+        document.body.appendChild(this.canvas);
+
+        // context 생성하기
+        this.ctx = this.canvas.getContext('2d');
+        
+        window.addEventListener('resize', this.resize.bind(this), false);
+        this.resize();
+
+    }   // end - constructor
+
+     // window 창에 따라 canva 사이즈 조절
+     resize() {
+        // body 의 너비 및 높이 저장하기
+        this.width = document.body.clientWidth;
+        this.height = document.body.clientHeight;
+
+        // 디스플레이 비율에 맞춰 canvas 사이즈와 비율 조정
+        this.canvas.width = this.width;
+        this.canvas.height = this.height;
+
+        // resize 할 경우 canvas clear 
+        this.ctx.clearRect(0, 0, this.width, this.height);
+    };
+
+}   // end - class
+
+window.onload = () => {
+    new App();
+}
